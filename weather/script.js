3541b11ae8d77b3daa97a0350b2b278b68fce01e
Irina <irbris@mail.ru>
2020-09-26 14:37:10 +0300

    fix: fix htpps

--- weather/script.js
+++ weather/script.js
@@ -4,7 +4,7 @@ const weatherDescription = document.querySelector('.weather-description');
 const city = document.querySelector('.city');
 
 async function getWeather() {
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city.textContent}&lang=ru&appid=08f2a575dda978b9c539199e54df03b0&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.textContent}&lang=ru&appid=08f2a575dda978b9c539199e54df03b0&units=metric`;
   const res = await fetch(url);
   const data = await res.json();
   
